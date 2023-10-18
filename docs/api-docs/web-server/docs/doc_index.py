1f8a18ec361e70899ec577e0ec1c7d50e48d2cb2
chenjr15 <chenjr15@live.cn>
2023-10-18 19:40:29 +0800

    feat: 根据资源分类

--- docs/api-docs/web-server/docs/doc_index.py
+++ docs/api-docs/web-server/docs/doc_index.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import argparse
 import sys
 
+from itertools import groupby
+
 parser = argparse.ArgumentParser(
     'docindex', description="a simple script for gather document index to markdown  readme file.")
 parser.add_argument('-s', '--source_dir', default='.', required=False)
@@ -36,11 +38,42 @@ class DocInfo:
     def __repr__(self) -> str:
         return str(self)
 
+    def types(self) -> str:
+
+        url = self.url
+        if url.startswith('/api/v1/'):
+            url = url[len('/api/v1/'):]
+        url_parts = url.split('/')
+        server = url_parts[0]
+
+        idx = 1
+        biz = 'res'
+        vendor = 'all_vendor'
+        resname = ''
+        while True:
+            if idx >= len(url_parts):
+                break
+            if url_parts[idx] == ("bizs"):
+                biz = 'biz'
+                idx += 1
+            elif url_parts[idx] == ("vendors"):
+                if url_parts[idx] == ("{vendor}"):
+                    vendor
+                idx += 1
+            else:
+                resname = url_parts[idx]
+                break
+            idx += 1
+        print(url_parts, "->", server, biz, vendor, resname)
+        return server,  resname, biz, vendor
+
 
 methods = ('GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'HEAD', 'OPTION')
 
 
 def extract(doc_file: Path) -> DocInfo:
+    '''返回单个文件的文档信息
+    '''
     info = DocInfo()
     info.filename = str(doc_file)
     with doc_file.open() as f:
@@ -65,6 +98,7 @@ def extract(doc_file: Path) -> DocInfo:
 
 
 def extract_all(source_dir: Path) -> list[DocInfo]:
+    '''获取指定目录下的所有文档信息'''
     infolist = list()
     for p in source_dir.glob('**/*.md'):
         if str(p).lower().endswith('readme.md'):
@@ -83,18 +117,41 @@ def main():
         print(f"source dir({source_dir}) not correct", file=sys.stderr)
         exit(1)
     out = t.open(mode='w', encoding='utf-8')
+    infos = extract_all(s)
+    infos.sort(key=DocInfo.types)
+    last_types = ('server', 'biz', 'vendor', 'res')
 
+    def print_title(n: int, types: tuple[str, str, str, str]):
+        for i in range(4-n, 4):
+            print(f"##{'#'*i} [{types[i]}]({types[i]})\n", file=out)
+
+    def diff_level(last: tuple, now: tuple) -> int:
+        for i in range(len(last)):
+            if last[i] != now[i]:
+                return len(last)-i
+        return 0
+
+    for info in infos:
+        types = info.types()
+        if types != last_types:
+            # 打印标题 和表头
+            print_title(diff_level(last_types, types), types)
+            print('| 方法| URL| 描述| 文档|', file=out)
+            print('|--------|--------|-----------------|--------|', file=out)
+        last_types = types
+        print(info, file=out)
+    # print([(t, len(list(data))) for t, data in result])
     # 按文件夹分类
-    for subdir in s.iterdir():
-        if not subdir.is_dir():
-            continue
-        print(f"## [{subdir}]({subdir})\n", file=out)
-        print('| 方法| URL| 描述| 文档|', file=out)
-        print('|--------|--------|-----------------|--------|', file=out)
-        infos = extract_all(subdir)
-        infos.sort(key=lambda x: x.url)
-        for info in infos:
-            print(info, file=out)
+    # for subdir in s.iterdir():
+    #     if not subdir.is_dir():
+    #         continue
+    #     print(f"## [{subdir}]({subdir})\n", file=out)
+    #     print('| 方法| URL| 描述| 文档|', file=out)
+    #     print('|--------|--------|-----------------|--------|', file=out)
+    #     infos = extract_all(subdir)
+    #     infos.sort(key=lambda x: x.url)
+    #     for info in infos:
+    #         print(info, file=out)
     out.close()
 
 
